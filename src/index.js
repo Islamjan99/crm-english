bc600a010553c39acf4c322e7a5a1570deba7f96
islam-aka <islamjan.m.m@gmail.com>
2022-08-05 16:52:54 +0600

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createContext } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './Global.css'
+import App from './App'
+import UserStore from './GlobalStore/UserStore'
+import AdminStore from './GlobalStore/AdminStore'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const Context = createContext(null)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+	<Context.Provider
+		value={{
+			user: new UserStore(),
+			device: new AdminStore(),
+		}}
+	>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Context.Provider>,
+	document.getElementById('root')
+)
